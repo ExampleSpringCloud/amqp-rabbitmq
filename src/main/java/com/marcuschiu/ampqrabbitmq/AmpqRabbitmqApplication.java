5022c2898b03f629555861d9785b0bbf257beb7a
Marcus Chiu <marcuschiu9@gmail.com>
2017-12-24 12:54:46 -0600

    RabbitTemolate

--- src/main/java/com/marcuschiu/ampqrabbitmq/AmpqRabbitmqApplication.java
+++ src/main/java/com/marcuschiu/ampqrabbitmq/AmpqRabbitmqApplication.java
@@ -1,12 +1,45 @@
 package com.marcuschiu.ampqrabbitmq;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@EnableBinding(Source.class)
+@RestController
 public class AmpqRabbitmqApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmpqRabbitmqApplication.class, args);
+	}
+
+//	@Bean
+//	@InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "5000", maxMessagesPerPoll = "1"))
+//	public MessageSource<String> orderSource() {
+//		return () -> {
+//			String str = "hello world";
+//			System.out.println("logging");
+//			return new GenericMessage<>(str);
+//		};
+//	}
+
+	@Autowired
+	private RabbitTemplate template;
+
+	@Value("${exchange.name}")
+	String exchangeName;
+
+	@Value("${routing.key:something}")
+	String routingKey;
+
+	@GetMapping("/")
+	public String send() {
+		String message = "Hello World";
+		this.template.convertAndSend(exchangeName, routingKey, message);
+		return "sent";
 	}
 }
